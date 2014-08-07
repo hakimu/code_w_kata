434d5c84e50b5fbced07b69647b1e726b929234a
Hakimu Jackson <hakimu@newrelic.com>
2014-08-06 22:42:55 -0700

    Refactor to make sure integer.  Add test file

--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -3,9 +3,15 @@
 # All values in the array will be integers. Return the good values in the order they are given.
 
 def no_odds(values)
-	values.select {|num| num.odd? }
+	values.select do |num|
+		if num.integer?
+			num.odd?
+		end
+	end
 end
 
-values_array = [1,2,3,4,5,6,7,8,9]
+
+values_array = [1,2,3,4,5,6,7.0,8,9,11.5]
 
 print no_odds(values_array)
+
